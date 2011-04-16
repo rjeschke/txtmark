99fe4060f6b277de115e264b467b2292ce56e37d
Rene' Jeschke <rene_jeschke@yahoo.de>
2011-04-16 15:18:06 +0200

    Improved Decorator, added JavaDoc for Decorator.

--- src/java/txtmark/Decorator.java
+++ src/java/txtmark/Decorator.java
@@ -1,40 +1,247 @@
+/*
+* Copyright (C) 2011 René Jeschke <rene_jeschke@yahoo.de>
+* See LICENSE.txt for licensing information.
+*/
 package txtmark;
 
+/**
+ * Decorator interface.
+ * 
+ * @author René Jeschke <rene_jeschke@yahoo.de>
+ */
 public interface Decorator
 {
+    /**
+     * Called when a paragraph is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;p>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openParagraph(final StringBuilder out);
+    
+    /**
+     * Called when a paragraph is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/p>\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeParagraph(final StringBuilder out);
 
+    /**
+     * Called when a blockquote is opened.
+     * 
+     * Default implementation is:
+     * <pre><code>out.append("&lt;blockquote>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openBlockquote(final StringBuilder out);
+    
+    /**
+     * Called when a blockquote is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/blockquote>\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeBlockquote(final StringBuilder out);
     
+    /**
+     * Called when a code block is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;pre>&lt;code>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openCodeBlock(final StringBuilder out);
+    
+    /**
+     * Called when a code block is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/code>&lt;/pre>\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeCodeBlock(final StringBuilder out);
     
+    /**
+     * Called when a code span is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;code>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openCodeSpan(final StringBuilder out);
+
+    /**
+     * Called when a code span is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/code>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeCodeSpan(final StringBuilder out);
     
+    /**
+     * Called when a headline is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code> out.append("&lt;h");
+     * out.append(level);
+     * out.append('>');</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openHeadline(final StringBuilder out, int level);
+    
+    /**
+     * Called when a headline is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code> out.append("&lt;/h");
+     * out.append(level);
+     * out.append(">\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeHeadline(final StringBuilder out, int level);
 
+    /**
+     * Called when a strong span is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;strong>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openStrong(final StringBuilder out);
+
+    /**
+     * Called when a strong span is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/strong>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeStrong(final StringBuilder out);
     
+    /**
+     * Called when an emphasis span is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;em>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openEmphasis(final StringBuilder out);
+    
+    /**
+     * Called when an emphasis span is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/em>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeEmphasis(final StringBuilder out);
 
+    /**
+     * Called when an ordered list is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;ol>\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openOrderedList(final StringBuilder out);
+    
+    /**
+     * Called when an ordered list is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/ol>\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeOrderedList(final StringBuilder out);
 
+    /**
+     * Called when an unordered list is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;ul>\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openUnorderedList(final StringBuilder out);
+    
+    /**
+     * Called when an unordered list is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/ul>\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeUnorderedList(final StringBuilder out);
     
+    /**
+     * Called when a list item is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;li>");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openListItem(final StringBuilder out);
+    
+    /**
+     * Called when a list item is closed.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;/li>\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void closeListItem(final StringBuilder out);
     
+    /**
+     * Called when a horizontal ruler is encountered.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;hr />\n");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void horizontalRuler(final StringBuilder out);
     
+    /**
+     * Called when a link is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;a");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openLink(final StringBuilder out);
     
+    /**
+     * Called when an image is opened.
+     * 
+     * <p>Default implementation is:</p>
+     * <pre><code>out.append("&lt;img");</code></pre>
+     * 
+     * @param out The StringBuilder to write to.
+     */
     public void openImage(final StringBuilder out);
 }
--- src/java/txtmark/DefaultDecorator.java
+++ src/java/txtmark/DefaultDecorator.java
@@ -1,56 +1,73 @@
+/*
+* Copyright (C) 2011 René Jeschke <rene_jeschke@yahoo.de>
+* See LICENSE.txt for licensing information.
+*/
 package txtmark;
 
+/**
+ * Default Decorator implementation.
+ * 
+ * @author René Jeschke <rene_jeschke@yahoo.de>
+ */
 public class DefaultDecorator implements Decorator
 {
-
+    /** @see txtmark.Decorator#openParagraph(StringBuilder) */
     @Override
     public void openParagraph(StringBuilder out)
     {
         out.append("<p>");
     }
 
+    /** @see txtmark.Decorator#closeParagraph(StringBuilder) */
     @Override
     public void closeParagraph(StringBuilder out)
     {
         out.append("</p>\n");
     }
 
+    /** @see txtmark.Decorator#openBlockquote(StringBuilder) */
     @Override
     public void openBlockquote(StringBuilder out)
     {
         out.append("<blockquote>");
     }
 
+    /** @see txtmark.Decorator#closeBlockquote(StringBuilder) */
     @Override
     public void closeBlockquote(StringBuilder out)
     {
         out.append("</blockquote>\n");
     }
 
+    /** @see txtmark.Decorator#openCodeBlock(StringBuilder) */
     @Override
     public void openCodeBlock(StringBuilder out)
     {
         out.append("<pre><code>");
     }
 
+    /** @see txtmark.Decorator#closeCodeBlock(StringBuilder) */
     @Override
     public void closeCodeBlock(StringBuilder out)
     {
         out.append("</code></pre>\n");
     }
 
+    /** @see txtmark.Decorator#openCodeSpan(StringBuilder) */
     @Override
     public void openCodeSpan(StringBuilder out)
     {
         out.append("<code>");
     }
 
+    /** @see txtmark.Decorator#closeCodeSpan(StringBuilder) */
     @Override
     public void closeCodeSpan(StringBuilder out)
     {
         out.append("</code>");
     }
 
+    /** @see txtmark.Decorator#openHeadline(StringBuilder, int) */
     @Override
     public void openHeadline(StringBuilder out, int level)
     {
@@ -59,6 +76,7 @@ public class DefaultDecorator implements Decorator
         out.append('>');
     }
 
+    /** @see txtmark.Decorator#closeHeadline(StringBuilder, int) */
     @Override
     public void closeHeadline(StringBuilder out, int level)
     {
@@ -67,78 +85,91 @@ public class DefaultDecorator implements Decorator
         out.append(">\n");
     }
 
+    /** @see txtmark.Decorator#openStrong(StringBuilder) */
     @Override
     public void openStrong(StringBuilder out)
     {
         out.append("<strong>");
     }
 
+    /** @see txtmark.Decorator#closeStrong(StringBuilder) */
     @Override
     public void closeStrong(StringBuilder out)
     {
         out.append("</strong>");
     }
 
+    /** @see txtmark.Decorator#openEmphasis(StringBuilder) */
     @Override
     public void openEmphasis(StringBuilder out)
     {
         out.append("<em>");
     }
 
+    /** @see txtmark.Decorator#closeEmphasis(StringBuilder) */
     @Override
     public void closeEmphasis(StringBuilder out)
     {
         out.append("</em>");
     }
 
+    /** @see txtmark.Decorator#openOrderedList(StringBuilder)*/
     @Override
     public void openOrderedList(StringBuilder out)
     {
         out.append("<ol>\n");
     }
 
+    /** @see txtmark.Decorator#closeOrderedList(StringBuilder) */
     @Override
     public void closeOrderedList(StringBuilder out)
     {
         out.append("</ol>\n");
     }
 
+    /** @see txtmark.Decorator#openUnorderedList(StringBuilder) */
     @Override
     public void openUnorderedList(StringBuilder out)
     {
         out.append("<ul>\n");
     }
 
+    /** @see txtmark.Decorator#closeUnorderedList(StringBuilder) */
     @Override
     public void closeUnorderedList(StringBuilder out)
     {
         out.append("</ul>\n");
     }
 
+    /** @see txtmark.Decorator#openListItem(StringBuilder) */    
     @Override
     public void openListItem(StringBuilder out)
     {
         out.append("<li>");
     }
 
+    /** @see txtmark.Decorator#closeListItem(StringBuilder) */
     @Override
     public void closeListItem(StringBuilder out)
     {
-        out.append("</li>");
+        out.append("</li>\n");
     }
 
+    /** @see txtmark.Decorator#horizontalRuler(StringBuilder) */
     @Override
     public void horizontalRuler(StringBuilder out)
     {
-        out.append("<hr />");
+        out.append("<hr />\n");
     }
 
+    /** @see txtmark.Decorator#openLink(StringBuilder) */
     @Override
     public void openLink(StringBuilder out)
     {
         out.append("<a");
     }
 
+    /** @see txtmark.Decorator#openImage(StringBuilder) */
     @Override
     public void openImage(StringBuilder out)
     {
